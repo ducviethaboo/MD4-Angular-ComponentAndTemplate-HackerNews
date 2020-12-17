23c92e8c83509d079346fe52e2ae48440c19d4d0
ducviethaboo <ducviet300397@gmail.com>
2020-12-17 14:44:37 +0700

    Commit 1

---
+++ hackernews/src/app/likes/likes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-likes',
+  templateUrl: './likes.component.html',
+  styleUrls: ['./likes.component.css'],
+})
+export class LikesComponent {
+  likes: number = 0;
+  likeThis() {
+    this.likes++;
+  }
+
+  dislike() {
+    this.likes--;
+  }
+}
